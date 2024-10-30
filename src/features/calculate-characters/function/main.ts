7add35f770c8ccfdf5f601ed014d7e8ea1649d88
antonio_mauricio <antonioimportant@gmail.com>
2024-10-30 10:51:20 -0300

    Adds calculating characters function

---
+++ src/features/calculate-characters/function/main.ts
@@ -0,0 +1,16 @@
+import { removeDashFromFlags } from "./../../../utils/remove-dash-from-flags.ts";
+import { splitInputFromFlags } from "./../../../utils/split-input-from-flags.ts";
+
+export function calculateCharacteres(args: string[]) {
+  let { input, flags } = splitInputFromFlags(args);
+
+  if (input === null || input.length === 0) {
+    throw new Error("Text cannot be empty.");
+  }
+
+  if (flags.length > 0 && removeDashFromFlags(flags).includes("no-spaces")) {
+    input = input.replaceAll(" ", "");
+  }
+
+  return "Characters Long: " + input?.trim().length;
+}
